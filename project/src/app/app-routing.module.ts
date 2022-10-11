962e89f07ea8f897a4700d95b0ad76ca6e8d0219
czaczka <89947179+czaczka@users.noreply.github.com>
2022-10-11 18:46:11 +1000

    added add user component

--- project/src/app/app-routing.module.ts
+++ project/src/app/app-routing.module.ts
@@ -1,9 +1,10 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AddUserComponent } from './add-user/add-user.component';
 import { LoginComponent } from './login/login.component';
 import { ProfileComponent } from './profile/profile.component';
 
-const routes: Routes = [{path: 'login', component:LoginComponent}, {path: 'profile', component:ProfileComponent}];
+const routes: Routes = [{path: 'login', component:LoginComponent}, {path: 'profile', component:ProfileComponent}, {path: 'add-user', component:AddUserComponent}];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- project/src/app/app.module.ts
+++ project/src/app/app.module.ts
@@ -7,12 +7,14 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { LoginComponent } from './login/login.component';
 import { ProfileComponent } from './profile/profile.component';
+import { AddUserComponent } from './add-user/add-user.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     LoginComponent,
-    ProfileComponent
+    ProfileComponent,
+    AddUserComponent
   ],
   imports: [
     BrowserModule,
