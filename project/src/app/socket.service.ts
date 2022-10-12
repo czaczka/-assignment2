05a78b425cd324e63a2c52c4dd97aff3daa9e586
czaczka <89947179+czaczka@users.noreply.github.com>
2022-10-12 21:32:58 +1000

    added sockets service

---
+++ project/src/app/socket.service.ts
@@ -0,0 +1,23 @@
+import { Injectable } from '@angular/core';
+import io from 'socket.io-client';
+import { Observable } from 'rxjs';
+
+const SERVER_URL = 'http://localhost:3000';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class SocketService {
+  private socket:any;
+  constructor() {this.socket = io(SERVER_URL);}
+  send(message: string){
+    this.socket.emit('message',message);
+    alert("sending:"+message);
+  }
+  getMessage(){
+    return new Observable(observer=>{
+      this.socket.on('message', (data:any)=> {observer.next(data);
+      });
+    });
+  }
+}
